54e75e552a23a9ef0d41ebba76accf945d100b7c
Nutnicha Chaliaosantikul <69999838+plalaeye@users.noreply.github.com>
2022-12-10 18:02:54 +0700

    🚧 Add bounce off screen

--- src/sim.ts
+++ src/sim.ts
@@ -1,53 +1,43 @@
-import type { Mass } from './mass'
+import type { Mass } from './mass';
 
 export class Sim {
-  private masses: Mass[] = []
-  private ctx: CanvasRenderingContext2D
-  private canvas: HTMLCanvasElement
+  private masses: Mass[] = [];
+  private ctx: CanvasRenderingContext2D;
+  private canvas: HTMLCanvasElement;
 
   constructor() {}
 
   addMass(mass: Mass) {
-    this.masses.push(mass)
+    this.masses.push(mass);
   }
 
   addMassMany(masses: Mass[]) {
-    this.masses.push(...masses)
+    this.masses.push(...masses);
   }
 
   start(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
-    this.ctx = ctx
-    this.canvas = canvas
-    setInterval(() => this.update(), 1000 / 60)
+    this.ctx = ctx;
+    this.canvas = canvas;
+    setInterval(() => this.update(), 1000 / 60);
   }
 
   update() {
     // this.removeMassesOutsideCanvas()
     for (const mass of this.masses) {
-      if (
-        !(
-          mass.pos.x > 0 &&
-          mass.pos.x < this.canvas.width &&
-          mass.pos.y > 0 &&
-          mass.pos.y < this.canvas.height
-        )
-      ) {
-        mass.vel.x = -mass.vel.x
-        mass.vel.y = -mass.vel.y
-      }
+      this.bounceMassesOffScreen(mass);
 
-      let total_force_x = 0
-      let total_force_y = 0
-      mass.isColliding = false
+      let total_force_x = 0;
+      let total_force_y = 0;
+      mass.isColliding = false;
       for (const other_mass of this.masses) {
         if (mass.pos.x === other_mass.pos.x && mass.pos.y == other_mass.pos.y)
-          continue
+          continue;
         const r = Math.sqrt(
           Math.pow(mass.pos.x - other_mass.pos.x, 2) +
             Math.pow(mass.pos.y - other_mass.pos.y, 2)
-        )
+        );
         if (r <= mass.r + other_mass.r) {
-          continue
+          continue;
         }
         // check if mass collides with other_mass
         // const r_squared =
@@ -85,9 +75,9 @@ export class Sim {
         // }
         // mass.vel.x = v_x !== 0 ? v_x : mass.vel.x
         // mass.vel.y = v_y !== 0 ? v_y : mass.vel.y
-        const { force_x, force_y } = this.calc_force(mass, other_mass)
-        total_force_x += force_x
-        total_force_y += force_y
+        const { force_x, force_y } = this.calc_force(mass, other_mass);
+        total_force_x += force_x;
+        total_force_y += force_y;
       }
       // let collide_x = false
       // let collide_y = false
@@ -125,14 +115,14 @@ export class Sim {
       // }
       // total_force_x = collide_x ? 0 : total_force_x
       // total_force_y = collide_y ? 0 : total_force_y
-      mass.pos.x += mass.vel.x
-      mass.pos.y += mass.vel.y
-      mass.vel.x += mass.acc.x
-      mass.vel.y += mass.acc.y
-      mass.acc.x = total_force_x / mass.kg
-      mass.acc.y = total_force_y / mass.kg
+      mass.pos.x += mass.vel.x;
+      mass.pos.y += mass.vel.y;
+      mass.vel.x += mass.acc.x;
+      mass.vel.y += mass.acc.y;
+      mass.acc.x = total_force_x / mass.kg;
+      mass.acc.y = total_force_y / mass.kg;
     }
-    this.draw()
+    this.draw();
   }
 
   private removeMassesOutsideCanvas() {
@@ -142,30 +132,39 @@ export class Sim {
         mass.pos.x < this.canvas.width &&
         mass.pos.y > 0 &&
         mass.pos.y < this.canvas.height
-      )
-    })
+      );
+    });
   }
 
   private calc_force(mass1: Mass, mass2: Mass) {
     // Gm1m2/r^2
-    const G = 6.67408e-11
-    const m1 = mass1.kg
-    const m2 = mass2.kg
+    const G = 6.67408e-11;
+    const m1 = mass1.kg;
+    const m2 = mass2.kg;
     const r_squared =
       Math.pow(mass1.pos.x - mass2.pos.x, 2) +
-      Math.pow(mass1.pos.y - mass2.pos.y, 2)
-    if (r_squared < 1) return { force_x: 0, force_y: 0 }
-    const r = Math.sqrt(r_squared)
-    const force = (G * m1 * m2) / r_squared
-    const force_x = (force * (mass2.pos.x - mass1.pos.x)) / r
-    const force_y = (force * (mass2.pos.y - mass1.pos.y)) / r
-    return { force_x, force_y }
+      Math.pow(mass1.pos.y - mass2.pos.y, 2);
+    if (r_squared < 1) return { force_x: 0, force_y: 0 };
+    const r = Math.sqrt(r_squared);
+    const force = (G * m1 * m2) / r_squared;
+    const force_x = (force * (mass2.pos.x - mass1.pos.x)) / r;
+    const force_y = (force * (mass2.pos.y - mass1.pos.y)) / r;
+    return { force_x, force_y };
+  }
+
+  private bounceMassesOffScreen(mass: Mass) {
+    if (!(mass.pos.x > 0 && mass.pos.x < this.canvas.width)) {
+      mass.vel.x = -mass.vel.x;
+    }
+    if (!(mass.pos.y > 0 && mass.pos.y < this.canvas.height)) {
+      mass.vel.y = -mass.vel.y;
+    }
   }
 
   private draw() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     for (const mass of this.masses) {
-      mass.draw(this.ctx)
+      mass.draw(this.ctx);
     }
   }
 }
